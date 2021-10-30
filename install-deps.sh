0f7198fc1a59269093fc1d29de6a56541ab82c81
Senal Bulumulle <senal.bulumulle@gmail.com>
2021-10-30 16:13:35 -0400

    Created the Dependencies Installer Script

---
+++ install-deps.sh
@@ -0,0 +1,27 @@
+## Install Electron globally
+npm install electron -g
+
+
+
+## Create a virtual environment
+virtualenv chatbot-environment
+
+
+## Change directory to chatbot-environment
+cd chatbot-environment
+
+
+## Activate it
+scripts/activate
+
+
+
+## Install the dependencies 
+pip3 install keras 
+pip3 install pillow
+pip3 install numpy
+pip3 install requests 
+pip3 install opencv-python
+
+
+
